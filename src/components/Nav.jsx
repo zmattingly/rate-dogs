6496ef0505da2e50213dc62640b86e5e55d44dc0
Zane Mattingly <zane.mattingly@quantcast.com>
2021-03-15 22:39:42 -0700

    Step 02: Pages with Link and naviagate nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
+import { Link, navigate } from 'gatsby'
 
 const Nav = () => {
+  const goToDogs = () => {
+    setTimeout(() => {
+      console.log('Going to Dogs after 2 seconds')
+      navigate('/dogs', { replace: true })
+    }, 2000)
+  }
+
   return (
     <nav>
       <ul>
         <li>
-          <a href="/dogs">Dogs</a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/about">About</a>
+          <Link to="/dogs">Dogs</Link>
         </li>
         <li>
-          <a href="/top">Top</a>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/top">Top</Link>
+        </li>
+        <li>
+          <button type="button" onClick={goToDogs}>
+            Go To Dogs
+          </button>
         </li>
       </ul>
     </nav>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
+import Nav from '../components/Nav'
+
 const HomePage = () => (
   <div>
+    <Nav />
     <h1>Home Page</h1>
   </div>
 )
